d1d1a5f8f15885fba6ab601017b793ab399a7678
Leo-xh <1134086740@qq.com>
2018-05-10 18:24:21 +0800

    5.10

--- RIP/RIPv2.py
+++ RIP/RIPv2.py
@@ -122,7 +122,7 @@ class RIPv2(object):
         sourceIp = utils.int2ip(sourceIp)
         sourceAddress = (sourceIp, sourcePort)
         data = data[10:]
-        for i in range(0, len(data), 21):
+        for i in range(0, len(data), 17):
             DestIp, DestPort, nextHopIp, nextHopPort, metric = struct.unpack("!IHIHB", data[i+4:i+17])
             DestIp = utils.int2ip(DestIp)
             nextHopIp = utils.int2ip(nextHopPort)
