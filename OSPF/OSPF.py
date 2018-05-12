d1f9b2b3475c4517748e74f0e041f4f905bd1ff8
Carl Xiao <carlxiaocx@163.com>
2018-05-12 17:21:03 +0800

    finish __dijkstra and __updateVector

--- OSPF/OSPF.py
+++ OSPF/OSPF.py
@@ -29,6 +29,7 @@ class OSPF(object):
         self.recvSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.recvSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.recvSocket.bind((self.address))
+        self.adjMatrix = {}
 
     
     def __begin(self):
@@ -103,6 +104,42 @@ class OSPF(object):
     def __sendLSU(self, address):
         pass
                   
+    def __dijkstra(self, neighbour):
+        dist = {neighbour:0}
+        dist.update(self.adjMatrix[neighbour])
+        # s = set([neighbour])
+        t = set(self.adjMatrix[neighbour].keys())
+        while len(t) > 0:
+            min_dist = None
+            nearest_dest = None
+            for dest in t:
+                if min_dist is None or dist[dest] < min_dist:
+                    min_dist = dist[dest]
+                    nearest_dest = dest
+            # s.add(nearest_dest)
+            t.remove(nearest_dest)
+            for (dest, dist_nd) in self.adjMatrix[nearest_dest]:
+                t.add(dest)
+                if dest not in dist or min_dist + dist_nd < dist[dest]:
+                    dest = min_dist + dist_nd
+        return dist
+
     def __updateVector(self, neighbour, neighbourVector):
-        pass
+        self.DistanceVectorLock.acquire()
+        self.adjMatrix[neighbour] = neighbourVector
+        neighbourDist = {}
+        selfDist = {}
+        for (nb, dist_nb) in self.neighbour.items():
+            neighbourDist[nb] = self.__dijkstra(nb)
+            for (dest, dist) in neighbourDist[nb].items():
+                if dest not in selfDist or dist_nb + dist < selfDist[dest]:
+                    selfDist[dest] = dist_nb + dist
+        for dest in selfDist:
+            self.distanceVector[dest] = []
+        for (nb, dist_nb) in self.neighbour.items():
+            nbDist = neighbourDist[nb]
+            for (dest, dist) in nbDist.items():
+                if selfDist[dest] == dist_nb + dist:
+                    self.distanceVector[dest].append(nb)
+        self.DistanceVectorLock.release()
                   
